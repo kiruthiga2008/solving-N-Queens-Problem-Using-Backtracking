2192ce372cc20dc8e8fd288016a44795819de02b
kiruthiga2008 <kiruthigas.cse2025@citchennai.net>
2026-07-21 21:00:54 +0530

    Update PROGRAM.py

--- PROGRAM.py
+++ PROGRAM.py
@@ -1,16 +1,12 @@
 def is_safe(board, row, col):
-    """
-    Check whether placing a queen at (row, col) is safe.
-    """
 
     for prev_row in range(row):
+
         placed_col = board[prev_row]
 
-        # Same column
         if placed_col == col:
             return False
 
-        # Same diagonal
         if abs(prev_row - row) == abs(placed_col - col):
             return False
 
@@ -18,13 +14,6 @@ def is_safe(board, row, col):
 
 
 def solve_n_queens(n):
-    """
-    Solve the N-Queens problem using backtracking.
-
-    Returns:
-        solutions: List of all valid solutions
-        backtrack_count: Number of backtracking operations
-    """
 
     board = [-1] * n
     solutions = []
@@ -32,24 +21,20 @@ def solve_n_queens(n):
 
     def backtrack(row):
 
-        # All queens placed successfully
         if row == n:
             solutions.append(board[:])
             return
 
-        # Try placing queen in every column
         for col in range(n):
 
             if is_safe(board, row, col):
 
-                # Place queen
                 board[row] = col
 
-                # Move to next row
                 backtrack(row + 1)
 
-                # Remove queen and backtrack
                 board[row] = -1
+
                 backtrack_count[0] += 1
 
     backtrack(0)
@@ -58,9 +43,6 @@ def solve_n_queens(n):
 
 
 def get_board(solution, n):
-    """
-    Convert a solution into a visual board.
-    """
 
     board = []
 
